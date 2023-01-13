cf4429c8aefd66d123d968464dfca6a6897a5980
Dan <dbulman68@gmail.com>
2023-01-13 19:36:51 +0000

    Started working on reader

--- app/src/main/java/bulman/daniel/nfcreader/MainActivity.java
+++ app/src/main/java/bulman/daniel/nfcreader/MainActivity.java
@@ -2,13 +2,72 @@ package bulman.daniel.nfcreader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.Ndef;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private NfcAdapter nfcAdapter;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        textView = findViewById(R.id.messageDisplay);
+
+        if (nfcAdapter == null) {
+            Toast.makeText(this, "This device doesn't support NFC!", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        IntentFilter[] intentFilters = new IntentFilter[]{};
+
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilters, null);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        nfcAdapter.disableForegroundDispatch(this);
+    }
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        Ndef ndef = Ndef.get(tag);
+
+        textView.setText("NFC Tag ID: " + bytesToHexString(tag.getId()));
+    }
+    private String bytesToHexString(byte[] src) {
+        StringBuilder stringBuilder = new StringBuilder("0x");
+        if (src == null || src.length <= 0) {
+            return null;
+        }
+
+        char[] buffer = new char[2];
+        for (byte b : src) {
+            buffer[0] = Character.forDigit((b >>> 4) & 0x0F, 16);
+            buffer[1] = Character.forDigit(b & 0x0F, 16);
+            stringBuilder.append(buffer);
+        }
+
+        return stringBuilder.toString();
     }
 }
